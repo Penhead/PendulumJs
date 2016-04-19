d427eff29513809d918d09299238dfedfbe15871
Penhead <penheadcreative@gmail.com>
2016-04-19 00:00:34 -0400

    added length functions

--- build/vector2.js
+++ build/vector2.js
@@ -5,12 +5,16 @@ function Vector2d(x, y) {
 
 //Public Methods
 Vector2d.prototype = {
+    lengthSqrd: function () {
+        return this.x*this.x + this.y * this.y;
+    },
+    length: function(){
+        return Math.sqrt(this.lengthSqrd());
+    },
     add: function (vec) {
         return new Vector2d(this.x + vec.x, this.y + vec.y);
     },
     subtract: function (vec) {
         return new Vector2d(this.x - vec.x, this.y - vec.y);
-        
     }
-    
 }
